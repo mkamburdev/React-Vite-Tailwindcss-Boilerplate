bb452737213c1b5831a9edb89bb7999cf2ffb2b9
mkamburdev <muhammed.kambur@dci-student.org>
2024-08-18 19:55:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+	return (
+		<>
+			<h1>React, Vite, Tailwind CSS Boilerplate</h1>
+			<p>
+				This boilerplate provides a quick and easy starting point for your
+				project. It includes essential configurations and dependencies to help
+				you get up and running in no time. Perfect for developers who want to
+				save time and focus on building features rather than setting up the
+				initial project structure.
+			</p>
+		</>
+	)
+}
+
+export default App
